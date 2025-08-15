ccf224d2ce38fd1b7f196a46a0a523a606751dc9
Gepiga29 <binimarygrace9@gmail.com>
2025-08-15 17:27:59 +0800

    Add files via upload

---
+++ laboratoryexercises/Problem2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratoryexercises;
+
+/**
+ *
+ * @author User
+ */
+public class Problem2 {
+    public static void main(String[] args) {
+      //calculate the sum
+       int num1 = 21;
+       int num2 = 32;   
+        System.out.println("53");  
+    }
+}
---
+++ laboratoryexercises/Program4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratoryexercises;
+
+/**
+ *
+ * @author User
+ */
+public class Program4 {
+    public static void main(String[] args) {
+        //test data
+        int a = -5 + 8* 6; // Operation a
+        int b = (55 + 9) % 9; // Operation b
+        int c = 28 + -3 * 5 / 8; // Operation c 
+        int d = 5 + 15 / 3 * 2 - 8 % 3; //Operation d
+        
+        // expected output
+        System.out.println(" " + a); // 43
+        System.out.println(" " + b); // 1
+        System.out.println(" " + c); // 19
+        System.out.println(" " + d); // 13
+    }
+}
