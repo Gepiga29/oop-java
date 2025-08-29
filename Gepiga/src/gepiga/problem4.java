e22f29678ad927cf9a10c26fae0110cf2f73838a
Gepiga29 <binimarygrace9@gmail.com>
2025-08-29 16:59:24 +0800

    Add files via upload

---
+++ Gepiga/src/gepiga/problem4.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gepiga;
+
+/**
+ *
+ * @author User
+ */
+public class problem4 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Gepiga/src/gepiga/problem5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gepiga;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class problem5 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Input an alphabet:  ");
+	  String alphabet = sc.next();
+       
+        if (alphabet.length()!= 1) {
+            System.out.println("error: input must be single character");
+        } else {
+            char letter = (alphabet.charAt(0));
+            if (letter >= 'a' && letter <= 'z') {
+                 if (letter == 'a' || letter == 'e' || letter == 'e' || letter == 'o' || letter == 'u') {
+                System.out.println(letter +" is a vowel");
+            } else {
+                System.out.println(letter +" is a consonant");
+            }
+        }
+      }
+    }
+}
+
